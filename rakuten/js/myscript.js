c2d0e7066d6ee57963f364200dcf1d5fb6ab9f70
Daisuke Watanabe <daigoro.white@gmail.com>
2012-12-31 17:31:23 +0900

    adjust many function , botton, change scroll

--- rakuten/js/myscript.js
+++ rakuten/js/myscript.js
@@ -1,108 +1,145 @@
 jQuery.noConflict();
 jQuery(document).ready(function(){
 
-//define variables
-var simple_tag='simple_rakuten_tag123456789'
-var original_page_bottom='for_original_page_display12345789'
-var wrap_all_item='wrap_all_item123456789'
+	//define variables
+	var simple_tag='simple_rakuten_tag123456789'
+	var original_page_bottom='for_original_page_display12345789'
+	var simple_page_bottom='for_simple_page_display12345789'
+	var wrap_all_item='wrap_all_item123456789'
+	var inited=false;
+	var taged=false;
 
-//main Item parts
-var flag_of_= jQuery(".susumeruArea")
-			.parent()
-			.parent()
-			.parent()
-			.parent()
-			.parent()
-			.parent()
-			.parent()
-			.parent()
+	//main Item parts
+	var flag_of_= jQuery(".susumeruArea")
+				.parent()
+				.parent()
+				.parent()
+				.parent()
+				.parent()
+				.parent()
+				.parent()
+				.parent()
 
-// number of item on the page
-var item_num=flag_of_.size();
+	// number of item on the page
+	var item_num=flag_of_.size();
 
-
-//check item number
-if( item_num != 0 && flag_of_ != null){
-//make new div for function"back to originalpage"
-	for(i=0;i<flag_of_.size(); i +=1){
-		flag_of_.eq(i)
-		.after(jQuery("<div class='"+ simple_tag + i + "'>"));
-	}
-	//make new div for wrap item tab		
-	jQuery("body").prepend("<div id ='" + wrap_all_item + "'>");
-
-	if(i == 1){
-		jQuery("#" + wrap_all_item ).prepend(flag_of_.first());
-	}else if(i >= 2){
-		//prepared about tabsUI
-		jQuery("#" + wrap_all_item ).prepend("<ul class='"+simple_tag+"'>");
-
-		// to tag
-		to_tagnization();
+	if (item_num != 0 && flag_of_ != null) {
+		main();
 	}
 
-	//hide any parts that is not needed
-	to_go_simple();
+	function main(){
+		//check item number
+		if(!inited ){
+		//make new div for function"back to originalpage"
+			for(i=0;i<flag_of_.size(); i +=1){
+				flag_of_.eq(i)
+				.after(jQuery("<div class='"+ simple_tag + i + "'>"));
+			}
+			//make new div for wrap item tab		
+			jQuery("body").prepend("<div id ='" + wrap_all_item + "'>");
 
-	//make link to back original page
-	make_back_link();
-}
+			//hide any parts that is not needed
+			to_go_simple();
+
+			inited=true;
+
+
+			//make link to simple page
+			make_to_simple_link();
+
+			//make link to back original page
+			make_back_link();
+
+			// hide to simple link
+			jQuery("."+ simple_page_bottom ).hide();
+		}
+	}
 
 	function to_tagnization(){
 		var li_tab_simple_rakuten;
 		var body_tab_simple_rakuten;
 		var a_tab_simple_rakuten;
 
-		//for tabsUI describe tab parent
-		for(i=0;i<flag_of_.size(); i +=1){
-			li_tab_simple_rakuten = (jQuery("<li>")).appendTo(jQuery("."+ simple_tag));
-			a_tab_simple_rakuten = jQuery("<a href='#tags-"+ i +"'>").appendTo(li_tab_simple_rakuten);
-			a_tab_simple_rakuten.text("Item"+(i+1));
+		// initialize tag
+		if(!taged){
+			//prepared about tabsUI
+			jQuery("#" + wrap_all_item ).prepend("<ul class='"+simple_tag+"'>");
+
+			//for tabsUI describe tab parent
+			for(i=0;i<flag_of_.size(); i +=1){
+				li_tab_simple_rakuten = (jQuery("<li>")).appendTo(jQuery("."+ simple_tag));
+				a_tab_simple_rakuten = jQuery("<a href='#tags-"+ i +"'>").appendTo(li_tab_simple_rakuten);
+				a_tab_simple_rakuten.text("Item"+(i+1));
+			}
+
+			// tab 
+			jQuery("#" + wrap_all_item ).tabs();
+
+			for(i=0;i<flag_of_.size(); i +=1){
+				// Add it white background color 
+				jQuery("#ui-tabs-"+(i+1))
+				.css("background-color","white");
+			}
+
+			//aTag link escape
+			jQuery("."+ simple_tag + " li" ).click(function() {
+		        return false;
+		    });
+
+			taged=true;
 		}
 
-		// tab 
-		jQuery("#" + wrap_all_item ).tabs();
-
+		//move to under tag
 		for(i=0;i<flag_of_.size(); i +=1){
-			// Add it white background color 
-			jQuery("#ui-tabs-"+(i+1))
-			.css("background-color","white");
-
 			// add item data to tag tab
 			flag_of_.eq(i)
-			.appendTo(jQuery("#ui-tabs-"+(i+1)))
+			.appendTo(jQuery("#ui-tabs-"+(i+1)));
 		}
+	}
 
-		//aTag link escape
-		jQuery("."+ simple_tag + " li" ).click(function() {
-	        return false;
-	    });
+	function do_tab(){
 
 	}
 
-	function to_go_simple(){
-		jQuery("body >  :gt(5)").hide();
-		jQuery("body").children().eq(5)
-		.prependTo(jQuery("body"));
-		
-		jQuery("body").children().eq(3)
-		.css("margin","20px 0 0 0")
-		.prependTo(jQuery("body"));
+	function make_to_simple_link(){
+		jQuery("body").prepend(
+			 jQuery("<a class='" + simple_page_bottom + "' id='change_botton'>")
+			.html("SIMPLE PAGE")
+			.click(function(){
+				to_go_simple();
+			})
+		);
+	}
 
+	function to_go_simple(){
+		if(i == 1){
+			jQuery("#" + wrap_all_item ).prepend(flag_of_.first());
+		}else if(i >= 2){
+			// to tag
+			to_tagnization();
+		}
+
+		//tag
+		jQuery("body >  :gt(5)").hide();
+		jQuery("body").children().eq(3)
+		.prependTo(jQuery("body"));
 		jQuery("#" + wrap_all_item ).show();
+
+		//botton
+		jQuery("."+ simple_page_bottom ).hide();
+		jQuery("body").prepend(jQuery("."+ original_page_bottom));
+		jQuery("."+ original_page_bottom ).show();
+
+		// scroll to top
+		window.scroll(0,0);
 	}
 
 
 
 	function make_back_link(){
 		jQuery("body").prepend(
-			 jQuery("<a class='" + original_page_bottom + "'>")
-			.html("back to original page")
-			.css("text-size","20px")
-			.css("font-size","15px")
-			.css("margin","20px 0 0 20px")
-			.css("color","blue")
-			.css("background-color","white")
+			 jQuery("<a class='" + original_page_bottom + "' id='change_botton'>")
+			.html("ORIGINAL PAGE")
 			.click(function(){
 				to_back_original();
 			})
@@ -110,13 +147,21 @@ if( item_num != 0 && flag_of_ != null){
 	}
 
 	function to_back_original(){
+		//tag
 		for(i=0;i<flag_of_.size(); i +=1){
 			jQuery("." + simple_tag + i)
 			.before(flag_of_.eq(i));
 		}
 		jQuery("body > *").css("display","");
 		jQuery("."+ original_page_bottom ).hide();
-		jQuery("#" + wrap_all_item ).hide();
+
+		//botton
+		jQuery("body").prepend(jQuery("."+ simple_page_bottom));
+		jQuery("."+ simple_page_bottom).show();
+		jQuery("#" + wrap_all_item).hide();
+
+		// scroll to top
+		window.scroll(0,0);
 	}
 
 });
